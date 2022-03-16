1271049e773517fec8afc29f74a9a83f272e8845
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-16 08:29:04 +0530

    Update roboshop/cart/

--- mutable/main.tf
+++ mutable/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source              = "git::https://github.com/raghudevopsb62/terraform-mutable-ec2"
+  source              = "git::https://github.com/raghudevopsb62/terraform-mutable-ec2?ref=v1.0.1"
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
   OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_TYPE  = var.SPOT_INSTANCE_TYPE
